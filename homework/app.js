d66c6af335360ba7672bd6cd1a80bfe3e447c605
AliakseiDziardziyashenka <leha.diorr@mail.ru>
2021-09-24 20:05:07 +0200

    m1

---
+++ homework/app.js
@@ -0,0 +1,20 @@
+VANTA.BIRDS({
+    el: "#border_home",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x000c1e,
+  color1: 0x825858,
+  color2: 0x115264,
+  birdSize: 0.60,
+  wingSpan: 40.00,
+  separation: 58.00,
+  alignment: 18.00,
+  cohesion: 55.00
+  
+  })
+  
